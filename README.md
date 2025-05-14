aa2f525223a6a2f74157fc712f2b2783fed5815a
kaungMyat-Khant <kmk.27.com@gmail.com>
2025-05-15 00:20:00 +0630

    update r script and readme md

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # data-Science-Capstone-Project
-This is the project for the data science specialization by Coursera. It involves the text prediction using the keyboard text data of Swift Key.
+
+This is the project for the data science specialization by Coursera. It involves the text prediction using the keyboard text data of Swift Key. Here is the data source: [swiftkey-data](%22https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip%22) \
+  
+## Mile stone report
+
+### Grading Criteria
+
+1.  Does the link lead to an HTML page describing the exploratory analysis of the training data set?
+
+2.  Has the data scientist done basic summaries of the three files? Word counts, line counts and basic data tables?
+
+3.  Has the data scientist made basic plots, such as histograms to illustrate features of the data?
+
+4.  Was the report written in a brief, concise style, in a way that a non-data scientist manager could appreciate?
--- cleanData.R
+++ cleanData.R
@@ -21,6 +21,7 @@ if(!file.exists("data/final/en_US")) {
 }
 
 
+
 ## read the blogs data
 con <- file("data/final/en_US/en_US.blogs.txt", open = "r")
 blogs <- readLines(con)
@@ -38,24 +39,36 @@ tweets <- readLines(con)
 close(con)
 
 
+# Libraries ---------------------------------------------------------------
+
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+# Summarize data ------------------------------------------------------------
+
 ## File size 
 Size <- c(blogs = format(object.size(blogs), units = "Mb"),
   news = format(object.size(news), units = "Mb"),
   tweets = format(object.size(tweets), units = "Mb"))
+gc()
 
 ## Number of lines
 Lines <- c(blogs = length(blogs),
            news = length(news),
            tweets = length(tweets))
+gc()
+
 
 ## Number of characters
 Characters <- c(blogs = sum(sapply(blogs, nchar)),
                 news = sum(sapply(news, nchar)),
                 tweets = sum(sapply(blogs, nchar)))
+gc()
 
 ## Numbers of words
 wordCount <- function(x) {
-  length(unlist(strsplit(x, split = "[:space:]")))
+  length(unlist(strsplit(x, split = " ")))
 }
 Words <- c(blogs = wordCount(blogs),
            news = wordCount(news),
@@ -63,5 +76,70 @@ Words <- c(blogs = wordCount(blogs),
 
 desdf <- data.frame(Lines, Characters,Words,Size)
 desdf
+gc()
+
+## Words per lines
+wordPerLine <- function(x) {
+    sapply(strsplit(x, split = " "), length)
+}
+
+
+blogs_wpl <-  wordPerLine(blogs)
+news_wpl <-  wordPerLine(news)
+tweets_wpl <- wordPerLine(tweets)
+
+blogHist <- ggplot(data = data.frame(wpl=blogs_wpl), aes(wpl))+ 
+    geom_histogram(fill = "navy", binwidth = 1)+
+    labs(x = "Number of Words per line",
+         title = "File: blogs")+
+    theme_bw()
+
+newsHist <- ggplot(data = data.frame(wpl=news_wpl), aes(wpl))+ 
+    geom_histogram(fill="orange", binwidth = 1)+
+    labs(x = "Number of Words per line",
+         title = "File: news")+
+    theme_bw()
+
+tweetsHist <- ggplot(data = data.frame(wpl=tweets_wpl), aes(wpl))+ 
+    geom_histogram(fill="skyblue", binwidth = 1)+
+    labs(x = "Number of Words per line",
+         title = "File: tweets")+
+    theme_bw()
+
+allHist <- ggplot(data = data.frame(wpl=c(blogs_wpl,news_wpl,tweets_wpl)), 
+                    aes(wpl))+ 
+    geom_histogram(fill="skyblue", binwidth = 1)+
+    labs(x = "Number of Words per line",
+         title = "File: all")+
+    theme_bw()
+
+cowplot::plot_grid(blogHist, newsHist, tweetsHist, allHist)
+
+max <- max(length(blogs_wpl),length(news_wpl), length(tweets_wpl))
+length(blogs_wpl) <- max
+length(news_wpl) <- max
+length(tweets_wpl) <- max
+
+words_per_line <- data.frame(blogs = blogs_wpl,
+                             news = news_wpl,
+                             tweets = tweets_wpl)
+rm("max")
+gc()
+
+
+
+words_per_line %>% 
+    summarise(across(everything(), list(Mean = mean, SD = sd, 
+                                        Median = median, IQR = IQR,
+                                        Max = max, Min = min), 
+                     na.rm = T)) %>%
+    mutate(across(c(ends_with("Mean"), ends_with("SD")), round)) %>% 
+    pivot_longer(
+    cols = everything(),
+    names_to = c("File", "statistic"),
+    names_sep = "_",
+    values_to = "value") %>%
+    pivot_wider(names_from = statistic, values_from = value)
+
 
 
